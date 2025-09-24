f989a58b3414d5cf081b46796e27e09841a509f2
misha <haumic@gmail.com>
2025-09-24 14:55:05 +0200

    vercel

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@ts-template/shared'],
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
